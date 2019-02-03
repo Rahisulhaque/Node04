6e34db8cab9a3babd12dfe238597c09c18c0f7ba
rahisulhaque <rahisul@ilcoud.com>
2019-02-03 00:14:13 -0700

    Start

---
+++ Method.java
@@ -0,0 +1,27 @@
+/* ***************************************************************************************** */
+/*                                                                                           */
+/*                                                                                           */
+/*   Method.java                                                     __    _            __   */
+/*                                                       _________ _/ /_  (_)______  __/ /   */
+/*   By: rahisulhaque <rahisul@icloud.com>              / ___/ __ `/ __ \/ / ___/ / / / /    */
+/*                                                     / /  / /_/ / / / / (__  ) /_/ / /     */
+/*   Created: 2019/02/02 23:44:28 by rahisulhaque     /_/   \__,_/_/ /_/_/____/___,_/_/      */
+/*   Updated: 2019/02/02 23:52:33 by rahisulhaque                                            */
+/*                                                                                           */
+/* ***************************************************************************************** */
+
+public class Method {
+    public static void main(String[] args) {
+
+        System.out.println(CadetNumber());
+    }
+
+
+    public static int CadetNumber() {
+        return 2125;
+    }
+
+    public static float Height(){
+        return 5.6;
+    }
+}
---
+++ com/company/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("This is a class!");
+        System.out.println(nothing());
+
+
+    }
+    public static int nothing(){
+        int i = 25;
+        return i;
+    }
+}
